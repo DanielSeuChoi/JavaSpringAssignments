03ec2c15e04a2096f1aa2f3ed9ca768535f63d18
DanielSeuChoi <choidanosa@gmail.com>
2023-02-10 23:26:25 -0800

    Implemented count query

--- HelloHuman/src/main/java/com/codingdojo/hellohuman/HomeController.java
+++ HelloHuman/src/main/java/com/codingdojo/hellohuman/HomeController.java
@@ -1,18 +1,23 @@
 package com.codingdojo.hellohuman;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestParam;
+
 @RestController
 @RequestMapping("/")
 
 public class HomeController {
 
 	@RequestMapping("")
-	public String index(@RequestParam(name="name",required=false)String searchQuery,@RequestParam(value="last_name",required=false)String lastQuery) {
+	public String index(
+			@RequestParam(name = "name", required = false) String searchQuery,
+			@RequestParam(value = "last_name", required = false) String lastQuery,
+			@RequestParam(value = "times", required = false) String countTime) {
 
-		return "First Name: "+searchQuery+"  " +"Last Name: "+lastQuery;
-		//,String searchQuery2
-		//,value=
-		//+"&"+ "Last Name: "+searchQuery2
+		return "First Name: " + searchQuery + "  " + "Last Name: " + lastQuery + "  " + "Time: " + countTime;
+		// ,String searchQuery2
+		// ,value=
+		// +"&"+ "Last Name: "+searchQuery2
 	}
 }
